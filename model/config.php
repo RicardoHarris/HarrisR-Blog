eb7d85ed6308dbc4ad0bd78ed07975280d5b887d
Ricardo Harris <ricardoharris@ymail.com>
2015-02-05 08:12:39 -0800

    8th commit, creating a database and refractoring my code

--- model/config.php
+++ model/config.php
@@ -1,2 +1,7 @@
 <?php
 $path = "/HarrisR-Blog/";
+
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database = "blog_db";
--- model/database.php
+++ model/database.php
@@ -1,6 +1,28 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "root";
-$database = "blog_db";
 
+class Database {
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    
+    public function __construct($host, $username, $password, $database){
+       $this->host = $host;
+       $this->username = $username;
+       $this->password = $password;
+       $this->database = $database;
+    }
+    
+    public function openConnection(){
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+    }
+    /*Establishes connection to database*/
+    public function closeConnection(){
+        
+    }
+    public function query($string){
+        
+    }
+    
+}
