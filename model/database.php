d2950522a28b81e8f89ac6fe7189d4eb0b264569
Ricardo Harris <ricardoharris@ymail.com>
2015-02-05 09:21:51 -0800

    9th commit, doing stuff

--- model/database.php
+++ model/database.php
@@ -1,28 +1,66 @@
 <?php
 
 class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
-    
-    public function __construct($host, $username, $password, $database){
-       $this->host = $host;
-       $this->username = $username;
-       $this->password = $password;
-       $this->database = $database;
+
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+
+        $this->connection = new mysqli($host, $username, $password);
+
+        if ($this->connection->connect_error) {
+            die("</p>Error: " . $this->connection->connect_error . "</p>");
+        }
+
+        $exists = $this->connection->select_db($database);
+
+        if (!$exists) {
+            $query = $this->connection->query("CREATE DATABASE $database");
+
+            if ($query) {
+                echo "<p>Successfully created database: " . $database . "</p>";
+            }
+        } else {
+            echo "<p>Database already exists.</p>";
+        }
     }
-    
-    public function openConnection(){
+
+    public function openConnection() {
         $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+            die("</p>Error: " . $connection->connect_error . "</p>");
+        }
     }
-    /*Establishes connection to database*/
-    public function closeConnection(){
-        
+
+    /* Establishes connection to database */
+
+    public function closeConnection() {
+        if (isset($this->connection)) {
+            $this->connection->close();
+        }
     }
-    public function query($string){
-        
+
+    /* Closes connection opened n "openConnection" function */
+
+    public function query($string) {
+        $this->openConnection();
+
+        $query = $this->connection->query($string);
+
+        $this->closeConnection();
+
+        return $query;
     }
-    
+
+    /* Opens connection to database, queries database, collects result whethter 
+     * successful, closes connection to database ,returns result */
 }
