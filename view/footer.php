7527805acd26a43d0eb645c98fcd5178efd2eb6c
Ricardo Harris <ricardoharris@ymail.com>
2015-02-20 09:18:45 -0800

    15th commit, first intermediate commit! Bootstrap and CSS down! Nav system up!

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,16 @@
-    </body>
+                
+</div>
+<div class="col-xs-2">
+    <nav class="list-group">
+        <a class="list-group-item" href="register.php">Register!</a>
+        <a class="list-group-item" href="login.php">Log in!</a>
+        <a class="list-group-item" href="post.php">Post!</a>
+        <a class="list-group-item" href="index.php">Posts!</a>
+        <a class="list-group-item" href="controller/logout-user.php">Log out!</a>
+    </nav>
+    <!--The "nav" here makes these words in black links to my other pages-->
+</div>
+</div>
+</div>
+</body>
 </html>
--- view/header.php
+++ view/header.php
@@ -1,6 +1,16 @@
 <html>
     <head>
+        <title>Commpunity</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="css/style.css">
     </head>
-    <body>
+    <body align="center">
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Commpunity</h1>
+            </div>
+            <div class="row">
+                <div class="col-xs-10">
