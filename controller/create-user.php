08fa630c8490dfb9cb0e9d2527121e36ff9c2352
Ricardo Harris <ricardoharris@ymail.com>
2015-02-24 09:16:36 -0800

    17th commit, working on advanced milestones, must update portfolio!

--- controller/create-user.php
+++ controller/create-user.php
@@ -10,11 +10,21 @@
     $hashedPassword = crypt($password, $salt);
     //Encrypts user password
     
-    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+   $query = $_SESSION["connection"]->query("SELECT username FROM users WHERE BINARY username = '$username'");
+   //Selects username from users table
+    
+    if($query->num_rows == 0){
+        echo "Successfully created user '$username'!";
+         $query2 = $_SESSION["connection"]->query("INSERT INTO users SET "
             . "email = '$email',"
             . "username = '$username',"
             . "password = '$hashedPassword',"
             . "salt = '$salt'");
+         //Inserts user data
+    }
+    else {
+        echo "Username already exists!";
+    }
     
     if($query) {
         echo "<p>" . $_SESSION["connection"]->error . "</p>";
